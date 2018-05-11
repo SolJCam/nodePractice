b560c7d9ae938dec444e43aa7f8766ef410c980d
SolJCam <scameron110@gmail.com>
2018-05-11 05:02:36 -0400

    http requests

--- file_renaming.js
+++ file_renaming.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-//fs.renameSync('./newFile.js', 'newDir/newFile2.js');
+//fs.renameSync('./newFile.js', 'newDir/newFile2.js');      //moved and renamed file
 
-fs.renameSync('./newDir/childDir', './parentDir');
+fs.renameSync('./newDir/childDir', './parentDir');      //moved and renamed directory
---
+++ http_requests.js
@@ -0,0 +1,38 @@
+const https = require('https');
+const fs = require('fs');
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+https.get(url, res => {
+
+    res.setEncoding('utf8');
+    let body = '';
+
+    res.on('data', data => {
+
+        body += data;
+
+    });
+
+    res.on('end', ()=>{
+
+        body = JSON.parse(body);
+
+        console.log(`${body[0].title}`);
+
+    });
+
+    res.on('end', ()=>{
+            //created file data.json with the 'body' written in it (utf8 encoding)
+        fs.writeFile('data.json', body, 'utf8', (err)=> {
+
+            if(err) return err;
+
+            console.log('just pulled all the posts and created file');
+
+        });
+
+    });
+
+});
+
