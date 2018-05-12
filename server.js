e85929ed932532f3545414e31bc259132f7b7d65
SolJCam <scameron110@gmail.com>
2018-05-11 23:17:06 -0400

    node server running on port 9111

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+            //first param: status code (200 = good); 2nd param: Header info (Content-Type 'key' : text/plain || text/html etc 'value') 
+    res.writeHead(200, {'Content-Type':'text/html'});
 
-    
+    res.end('<h1>Hi guys, it\'s Sol!</h1>')
 
 });
+
+server.listen(9111);
+
+console.log('Our server is running');
