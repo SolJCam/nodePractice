522cbe20b2fc137648e50a50ede2528b560c1725
SolJCam <scameron110@gmail.com>
2018-05-13 04:10:30 -0400

    serving json completed

--- http_serving_json.js
+++ http_serving_json.js
@@ -6,29 +6,28 @@ const url = 'https://jsonplaceholder.typicode.com/posts';
 
 http.createServer((req, serverRes) => {
 
+        //http.createServer outgoing msg
+    //console.log(serverRes);
+        //http.createServer incoming msg
+    //console.log(req);
+
     if(req.method === 'GET' && req.url === '/posts'){
-
         https.get(url, (httpRes)=>{
-
             httpRes.on('data', data => {
-
                 httpRes.setEncoding('utf8');
-
-                console.log(data);
-
+                //console.log(data);
+                serverRes.write(data);
             });
 
-                //'end' event keyword: runs at completion of 'data' event
+                //'end' event keyword: runs at completion of 'data' event; ends 'resp' NOT connection
             httpRes.on('end', ()=>{
-
                 serverRes.end();
-
-                console.log('its over')
-
+                //console.log('its over')
             });
-
         })
-
+    }else{
+        serverRes.writeHead(404, {'Content-Type': 'text/plain'});
+        serverRes.end('404 ERROR, could not find what you were looking for');
     }
 
 
