f7b1da0aa4d171358d8cc3c56fc3a07303f2c50a
SolJCam <scameron110@gmail.com>
2018-05-13 04:29:10 -0400

    form submission

---
+++ http_form_submit.js
@@ -0,0 +1,13 @@
+const http = requrire('http');
+const fs = require('fs');
+
+http.createSrerver(((req, resp)=>{
+
+    let body = '';
+
+    if(req.method === 'GET'){
+
+
+    }
+
+}).listen(4444);
