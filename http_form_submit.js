184407a435fcc5991d96c1a1afb6730a66d65d26
SolJCam <scameron110@gmail.com>
2018-05-14 20:50:38 -0400

    POST server request

--- http_form_submit.js
+++ http_form_submit.js
@@ -1,13 +1,35 @@
-const http = requrire('http');
+const http = require('http');
 const fs = require('fs');
 
-http.createSrerver(((req, resp)=>{
+http.createServer((req, res)=>{
 
     let body = '';
 
     if(req.method === 'GET'){
+        //console.log(req.method);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        //file system reads file and returns it to server as 'data' which is then sent via 'res' to browser
+        fs.readFile('./http-form.html', 'UTF-8', (err, data) => {
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        });
+    }else if(req.method === 'POST'){
 
+        req.on('data', data => {
+            body+= data;
+        });
 
+        req.on('end', ()=>{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(body, ()=>{
+                res.end();
+            });
+        })
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('404 ERROR could not find what you were looking for');
     }
+}).listen(9111);
 
-}).listen(4444);
+console.log('it\'s working');
