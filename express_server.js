4f32351cedc2cffb8c764604e244ea140acc45e7
SolJCam <scameron110@gmail.com>
2018-05-18 23:18:50 -0400

    using express server

--- express_server.js
+++ express_server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
 let app = express();
 
-app.listen(9999);
+//When in production and hosting on another service (ex. Heroku or AWS), the host may configure the process.env.PORT variable to tell your web server what port to listen on; 
+//after all, your script runs in their environment.
+const port = process.env.PORT || 9999;
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api', (req, res)=>{
+    //res.send('<h1>Api Page</h1>');
+    res.json({name: "Sol"});
+});
+
+
+app.listen(port);
 
 console.log('It\'s working');
